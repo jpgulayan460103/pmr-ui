4cfca54efd57508d03f9c73d8f5aff315836c445
John Paul Gulayan <jpgulayan@gmail.com>
2022-07-19 09:26:19 +0800

    add icon components

--- src/Pages/Inventory/Inventory.jsx
+++ src/Pages/Inventory/Inventory.jsx
@@ -12,6 +12,7 @@ import Icon, {
 } from '@ant-design/icons';
 import helpers from '../../Utilities/helpers';
 import AuditBatches from '../../Components/AuditBatches';
+import MaximizeSvg from '../../Icons/MaximizeSvg';
 
 const { Option, OptGroup } = Select;
 
@@ -31,11 +32,7 @@ function mapStateToProps(state) {
     };
 }
 
-const MaximizeSvg = () => (
-    <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 1024 1024">
-        <path d="M794.432 983.552H51.2a25.6 25.6 0 0 1-25.6-25.6V214.784a25.6 25.6 0 0 1 25.6-25.6h152.768V66.112a25.6 25.6 0 0 1 25.6-25.6H972.8a25.6 25.6 0 0 1 25.6 25.6v743.232a25.6 25.6 0 0 1-25.6 25.6h-152.768v123.008a25.6 25.6 0 0 1-25.6 25.6z m-717.632-51.2h692.032V240.384H76.8v691.968z m743.232-148.672H947.2V91.648H255.168v97.472h539.264a25.6 25.6 0 0 1 25.6 25.6v568.96z" p-id="2528"></path>
-    </svg>
-);
+
 
 const Inventory = (props) => {
     const formRef = React.useRef();
